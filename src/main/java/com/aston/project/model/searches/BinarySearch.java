ca51105ed5ce1c8df2c40f8233cc5842c7e85646
colgens <colgens@gmail.com>
2024-08-11 20:46:12 +0700

    Add Javadoc and update BinarySearch method: - Add detailed Javadoc comments for the search method - Change return type to return found element instead of index

--- src/main/java/com/aston/project/model/searches/BinarySearch.java
+++ src/main/java/com/aston/project/model/searches/BinarySearch.java
@@ -1,22 +1,37 @@
 package com.aston.project.model.searches;
 
 import java.util.Comparator;
+import java.util.function.Function;
 
+/**
+ * Класс для выполнения бинарного поиска в отсортированном массиве.
+ */
 public class BinarySearch {
-    public <T> int search(T[] arr, T key, Comparator<T> comparator) {
+    /**
+     * Выполняет бинарный поиск элемента в отсортированном массиве.
+     *
+     * @param <T> тип элементов в массиве
+     * @param <S> тип искомого элемента
+     * @param arr массив, в котором выполняется поиск
+     * @param key искомый элемент
+     * @param getter функция для получения значения из элемента массива, которое будет сравниваться с искомым элементом
+     * @param comparator компаратор для сравнения значений, полученных с помощью функции getter
+     * @return найденный элемент массива, если он найден; null, если элемент не найден
+     */
+    public <T, S> T search(T[] arr, S key, Function<T, S> getter, Comparator<S> comparator) {
         int low = 0;
         int high = arr.length - 1;
         while (low <= high) {
             int mid = (low + high) / 2;
-            int resultOfCompare = comparator.compare(arr[mid], key);
+            int resultOfCompare = comparator.compare(getter.apply(arr[mid]), key);
             if (resultOfCompare == 0) {
-                return mid;
+                return arr[mid];
             } else if (resultOfCompare < 0) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
-        return -1;
+        return null;
     }
 }
