01532cddaf7360770bc75cd268d1c90b0c18ecc8
colgens <colgens@gmail.com>
2024-08-11 21:59:45 +0700

    Update BinarySearch method: - Remove comparator parameter as key type is now assumed to be Comparable - Update Javadoc comments to reflect the changes

--- src/main/java/com/aston/project/model/searches/BinarySearch.java
+++ src/main/java/com/aston/project/model/searches/BinarySearch.java
@@ -1,6 +1,5 @@
 package com.aston.project.model.searches;
 
-import java.util.Comparator;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -12,19 +11,18 @@ public class BinarySearch {
      * Выполняет бинарный поиск элемента в отсортированном массиве.
      *
      * @param <T> тип элементов в массиве
-     * @param <S> тип искомого элемента
+     * @param <S> тип искомого элемента, должен реализовывать Comparable
      * @param arr массив, в котором выполняется поиск
      * @param key искомый элемент
      * @param getter функция для получения значения из элемента массива, которое будет сравниваться с искомым элементом
-     * @param comparator компаратор для сравнения значений, полученных с помощью функции getter
      * @return Optional, содержащий найденный элемент массива, если он найден; Optional.empty(), если элемент не найден
      */
-    public <T, S> Optional<T> search(T[] arr, S key, Function<T, S> getter, Comparator<S> comparator) {
+    public <T, S extends Comparable<? super S>> Optional<T> search(T[] arr, S key, Function<T, S> getter) {
         int low = 0;
         int high = arr.length - 1;
         while (low <= high) {
             int mid = (low + high) / 2;
-            int resultOfCompare = comparator.compare(getter.apply(arr[mid]), key);
+            int resultOfCompare = getter.apply(arr[mid]).compareTo(key);;
             if (resultOfCompare == 0) {
                 return Optional.of(arr[mid]);
             } else if (resultOfCompare < 0) {
@@ -36,3 +34,5 @@ public class BinarySearch {
         return Optional.empty();
     }
 }
+
+
